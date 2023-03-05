8fdc1cbc89cb5a74e94263116fa809d8f3f162a7
EussyKR <hang3137@naver.com>
2023-03-05 21:58:21 +0900

    feat : Effect 데이터 리소스화에 따른 id를 통한 C_Effect 초기화 코드 추가

--- EffectTool/src/FX_Effect.cpp
+++ EffectTool/src/FX_Effect.cpp
@@ -4,12 +4,15 @@
 
 using namespace KGCA41B;
 
-void FX_Effect::OnInit(entt::registry& registry, AABBShape collision_box, C_Effect effect)
+void FX_Effect::OnInit(entt::registry& registry, AABBShape collision_box, string effect_id = "")
 {
 	FX_BaseEffectActor::OnInit(registry, collision_box);
 
-	effect.local = XMMatrixIdentity();
-	auto& effect_comp = registry.emplace<C_Effect>(entity_id_, effect);
+	C_Effect initial_effect_comp;
+	auto& effect_comp = registry.emplace<C_Effect>(entity_id_, initial_effect_comp);
+	effect_comp.local = XMMatrixIdentity();
+	effect_comp.effect_id = effect_id;
+	effect_comp.effect = *RESOURCE->UseResource<Effect>(effect_id);
 
 	transform_tree_.AddNodeToNode(TYPE_ID(KGCA41B::C_Transform), TYPE_ID(KGCA41B::C_Effect));
 	transform_tree_.root_node->OnUpdate(registry, entity_id_);
@@ -21,14 +24,14 @@ void FX_Effect::OnUpdate(entt::registry& registry)
 	
 }
 
-void FX_Effect::AddEmitter(entt::registry& registry, shared_ptr<Emitter> emitter)
+void FX_Effect::AddEffect(entt::registry& registry, map<string, Emitter>& emitter_list)
 {
-	auto& effect = registry.get<C_Effect>(entity_id_);
-	effect.emitters.push_back(emitter);
+	auto& effect_comp = registry.get<C_Effect>(entity_id_);
+	effect_comp.effect.emitters = emitter_list;
 }
 
-void KGCA41B::FX_Effect::ResetEmitter(entt::registry& registry)
+void FX_Effect::ResetEmitter(entt::registry& registry)
 {
-	auto& effect = registry.get<C_Effect>(entity_id_);
-	effect.emitters.clear();
+	auto& effect_comp = registry.get<C_Effect>(entity_id_);
+	effect_comp.effect.emitters.clear();
 }
--- EffectTool/src/FX_Effect.h
+++ EffectTool/src/FX_Effect.h
@@ -5,10 +5,10 @@ namespace KGCA41B {
 	class FX_Effect : public FX_BaseEffectActor
 	{
 	public:
-		virtual void OnInit(entt::registry& registry, AABBShape collision_box, C_Effect effect);
+		virtual void OnInit(entt::registry& registry, AABBShape collision_box, string effect_id = "");
 		virtual void OnUpdate(entt::registry& registry);
 	public:
-		void AddEmitter(entt::registry& registry, shared_ptr<Emitter> emitter);
+		void AddEffect(entt::registry& registry, map<string, Emitter>& emitter_list);
 		void ResetEmitter(entt::registry& registry);
 	};
 }
